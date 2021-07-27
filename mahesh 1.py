c27adfe3832c2c9e5e55524d6dc0830fe66b8fb6
Mahesh14141 <77565634+Mahesh14141@users.noreply.github.com>
2021-07-27 18:04:21 +0530

    Add files via upload

---
+++ mahesh 1.py
@@ -0,0 +1,28 @@
+"""
+#print("hello world",end=" ")
+var1=("hello")
+var2=45
+var3=55.5
+var4=()
+var5="aaa"
+#print(type(var4))
+print(str(var2)+var5 )
+
+"""
+"""
+#input() function :-
+#print("enter a number")
+#name=input()
+#print("you enter is" ,name)
+xyz=input("enter your age")
+print("your age is ", xyz)
+
+"""
+var1="my name is "
+var2="mahesh"
+var3=5.0
+var4="45"
+print(float(var3)+float(var4))
+
+print(str(var1)+str(var4))
+
